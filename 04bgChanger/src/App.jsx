0e6063a76fd18c4ae8599d7c4460a03809246cce
Rohit <rhtshere@gmail.com>
2025-12-21 22:00:59 +0530

    bg-changer project added

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'red' }}
+          >
+            Red
+          </button>
+
+          <button
+            onClick={() => setColor("indigo")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'indigo' }}
+          >
+            Indigo
+          </button>
+
+          <button
+            onClick={() => setColor("olive")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'olive' }}
+          >
+            Olive
+          </button>
+
+          <button
+            onClick={() => setColor("grey")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: 'grey' }}
+          >
+            Grey
+          </button>
+
+          <button
+            onClick={() => setColor("brown")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "brown" }}
+          >
+            Brown
+          </button>
+
+          <button
+            onClick={() => setColor("teal")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "teal" }}
+          >
+            Teal
+          </button>
+
+          <button
+            onClick={() => setColor("orange")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </button>
+
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
