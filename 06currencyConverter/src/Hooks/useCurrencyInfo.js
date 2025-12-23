19abeffcd94de2a2dfc431e3cd0b3189ed2893d6
Rohit <rhtshere@gmail.com>
2025-12-23 11:34:20 +0530

    06 Currency Coverter complete

---
+++ 06currencyConverter/src/Hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from "react"
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://api.exchangerate-api.com/v4/latest/usd${currency}`)
+        .then((res) => res.json())
+        .then((res) => setData(res['rates']))
+        console.log(data);
+    }, [currency])
+    console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
